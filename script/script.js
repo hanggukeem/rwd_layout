07ba9bcdbc50a8ef3d6f660d0763ec97bfada08c
hanggukeem <o1b.hannah@gmail.com>
2020-09-21 17:48:45 +0900

    rwd website 구현하기

---
+++ script/script.js
@@ -0,0 +1,21 @@
+//라이센스키
+ $("#fullpage").fullpage({
+	 licensekey: 'OPEN-SOURCE-GPLV3-LICENSE',
+	 autoScrolling:true,
+	 scrollHorizontally:true,
+	 sectionsColor: ['#e6c7c7', '#e6daa8', '#7BAABE', 'whitesmoke', '#000']
+ });
+//mainslide
+
+//        setInterval(fnSlide, 2000);
+//
+//        function fnSlide() {
+//
+//            $("#main>div:first")
+//                .fadeOut(2000, function() {
+//                    $(this).insertAfter("#main>div:last");
+//                });
+//
+//            $("#main>div:nth-child(2)").fadeIn(2000);
+//
+//        }
